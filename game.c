8cfd54965ef9e953c75bacc0ad9ec6e5adabf7df
Bjorn Nah <bjorn.nah@gmail.com>
2021-12-09 23:45:58 +0100

    add multiples holes

--- game.c
+++ game.c
@@ -7,7 +7,7 @@
 
 #include "game.h"
 
-unsigned int playing, level;
+unsigned int playing, level, holes;
 unsigned char game_status, gates;
 unsigned int randomizator2 = 0;
 
@@ -18,6 +18,7 @@ extern unsigned char explorer01_spr[];
 extern unsigned char explorer02_spr[];
 extern unsigned char explorer03_spr[];
 extern unsigned char hole00_spr[];
+extern unsigned char hole01_spr[];
 extern unsigned char wall_u0[];
 extern unsigned char wall_u1[];
 extern unsigned char wall_d0[];
@@ -64,7 +65,7 @@ SCB_REHV_PAL explorer_spr = {
   {0xD1,0x23,0x45,0x67,0x89,0xAB,0xC0,0xEF}
 };
 
-SCB_REHV_PAL hole = {
+SCB_REHV_PAL hole0 = {
   BPP_4 | TYPE_NORMAL, 
   REHV,
   0x01,
@@ -76,11 +77,35 @@ SCB_REHV_PAL hole = {
   {0xD1,0x23,0x45,0x67,0x89,0xAB,0xC0,0xEF}
 };
 
+SCB_REHV_PAL hole1 = {
+  BPP_4 | TYPE_NORMAL, 
+  REHV,
+  0x01,
+  (char *)&hole0,
+  hole00_spr,
+  40, 40,
+  0x0100, 0x0100,
+  // 0 and D are inverted to make magenta transluent
+  {0xD1,0x23,0x45,0x67,0x89,0xAB,0xC0,0xEF}
+};
+
+SCB_REHV_PAL hole2 = {
+  BPP_4 | TYPE_NORMAL, 
+  REHV,
+  0x01,
+  (char *)&hole1,
+  hole00_spr,
+  100, 80,
+  0x0100, 0x0100,
+  // 0 and D are inverted to make magenta transluent
+  {0xD1,0x23,0x45,0x67,0x89,0xAB,0xC0,0xEF}
+};
+
 SCB_REHV_PAL wall_l = {
   BPP_4 | TYPE_NORMAL, 
   REHV,
   0x01,
-  (char *)&hole,
+  (char *)&hole2,
   wall_l0,
   0, 0,
   0x0100, 0x0100,
@@ -150,25 +175,39 @@ void init_level(){
 	unsigned int pop = 1;
 	
 	while(pop){
-		hole.hpos = rand()/341 + 32;
-		hole.vpos = rand()/862 + 32;			
-		vdiff = explorer_spr.vpos - hole.vpos;
-		hdiff = explorer_spr.hpos - hole.hpos;
+		hole0.hpos = rand()/341 + 32;
+		hole0.vpos = rand()/862 + 32;			
+		vdiff = explorer_spr.vpos - hole0.vpos;
+		hdiff = explorer_spr.hpos - hole0.hpos;
 		if (!(vdiff > -16 && vdiff < 4 && hdiff > -8 && hdiff < 8)){
 			pop = 0;
 		}
 	}
 	
-	if(level < 4){gates = UP_GATE + DOWN_GATE + LEFT_GATE + RIGHT_GATE;}
-	if(level >= 4 && level < 8){gates = UP_GATE + DOWN_GATE;}
-	if(level >= 8){gates = UP_GATE + DOWN_GATE;}
+	if(level < 4){
+		gates = UP_GATE + DOWN_GATE + LEFT_GATE + RIGHT_GATE;
+		holes = 1;
+		hole1.data = hole01_spr;
+		hole2.data = hole01_spr;
+	}
+	if(level >= 4 && level < 8){
+		gates = rand() & 0x000F;
+		holes = 2;
+		hole1.data = hole00_spr;
+		hole2.data = hole01_spr;
+		}
+	if(level >= 8){
+		gates = rand() & 0x000F;
+		holes = 3;
+		hole1.data = hole00_spr;
+		hole2.data = hole00_spr;
+	}
 	
 	/*
 	if(gates == UP_GATE + DOWN_GATE + LEFT_GATE + RIGHT_GATE){walls.data = walls00;}
 	if(gates == LEFT_GATE + RIGHT_GATE){walls.data = walls01;}
 	if(gates == UP_GATE + DOWN_GATE){walls.data = walls02;}
 	*/
-	//if(gates & UP_GATE){wall_u.data = wall_u0;}else{wall_u.data = wall_u1;}
 	wall_u.data = (gates & UP_GATE) 	? wall_u0 : wall_u1;
 	wall_d.data = (gates & DOWN_GATE) 	? wall_d0 : wall_d1;
 	wall_l.data = (gates & LEFT_GATE) 	? wall_l0 : wall_l1;
@@ -306,21 +345,23 @@ void explorer_logic(){
 
 void physics(){
 	signed int vdiff, hdiff;
-	vdiff = explorer_spr.vpos - hole.vpos;
-	hdiff = explorer_spr.hpos - hole.hpos;
+	vdiff = explorer_spr.vpos - hole0.vpos;
+	hdiff = explorer_spr.hpos - hole0.hpos;
 	if ((vdiff > -14 && vdiff < 2) && (hdiff > -8 && hdiff < 8) && explorer.status != FALLING){
 		explorer.status = FALL;
 	}
-	if(explorer_spr.hpos == 80 && explorer_spr.vpos<17 && (gates & UP_GATE)){
+	
+	// gates
+	if(explorer_spr.hpos > 72 && explorer_spr.hpos < 88 && explorer_spr.vpos<10 && (gates & UP_GATE)){
 		game_status = LEVEL_UP;
 	}
-	if(explorer_spr.hpos == 80 && explorer_spr.vpos>93 && (gates & DOWN_GATE)){
+	if(explorer_spr.hpos > 72 && explorer_spr.hpos < 88 && explorer_spr.vpos>92 && (gates & DOWN_GATE)){
 		game_status = LEVEL_UP;
 	}
-	if(explorer_spr.hpos < 17 && explorer_spr.vpos == 51 && (gates & LEFT_GATE)){
+	if(explorer_spr.hpos < 10 && explorer_spr.vpos > 43 && explorer_spr.vpos < 59 && (gates & LEFT_GATE)){
 		game_status = LEVEL_UP;
 	}
-	if(explorer_spr.hpos > 143 && explorer_spr.vpos == 51 && (gates & RIGHT_GATE)){
+	if(explorer_spr.hpos > 150 && explorer_spr.vpos > 43 && explorer_spr.vpos < 59 && (gates & RIGHT_GATE)){
 		game_status = LEVEL_UP;
 	}
 	
@@ -337,7 +378,9 @@ void game_logic(){
 	tgi_sprite(&wall_u);
 	tgi_sprite(&wall_l);
 	tgi_sprite(&wall_r);
-	tgi_sprite(&hole);
+	tgi_sprite(&hole0);
+	tgi_sprite(&hole1);
+	tgi_sprite(&hole2);
 	tgi_sprite(&explorer_spr);
 	tgi_sprite(&ghost);
 	tgi_sprite(&wall_d);
